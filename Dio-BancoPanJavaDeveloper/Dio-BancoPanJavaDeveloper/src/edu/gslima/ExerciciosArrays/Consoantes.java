4797a8970cf07bf56413fbc83883021011cf78f2
gslima <slg60277@gmail.com>
2023-02-22 00:03:12 -0300

    atividade: array contagem de consoantes

--- Dio-BancoPanJavaDeveloper/Dio-BancoPanJavaDeveloper/src/edu/gslima/ExerciciosArrays/Consoantes.java
+++ Dio-BancoPanJavaDeveloper/Dio-BancoPanJavaDeveloper/src/edu/gslima/ExerciciosArrays/Consoantes.java
@@ -2,26 +2,4 @@ package edu.gslima.ExerciciosArrays;
 
 public class Consoantes {
 
-    public static void main(String[] args) {
-
-
-        char[] letras = {'a', 'c', 'd', 'j', 'i', 'p'};
-        int vogais=0, consoantes=0;
-        int i = 0;
-
-        do{
-
-            if((letras[i] != 'a') && (letras[i] != 'e') && (letras[i] != 'i') && (letras[i] != 'o') && (letras[i] != 'u')){
-                consoantes++;
-            }else{
-                vogais++;
-            }
-            i++;
-        }while(i < letras.length);
-
-        System.out.println("O total de vogais é: "+vogais);
-        System.out.println("O total de consoantes é: "+consoantes);
-
-    }
-
 }
